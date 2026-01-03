b669a2bfa3e0bf4415f3bd2611fdb1e87dcf529a
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-01-03 12:13:36 +0530

    adding the initial archetecture structure

--- app/src/main/java/com/nigdroid/focusflow_nitr/ui/main/MainActivity.kt
+++ app/src/main/java/com/nigdroid/focusflow_nitr/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.nigdroid.focusflow_nitr
+package com.nigdroid.focusflow_nitr.ui.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.nigdroid.focusflow_nitr.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
